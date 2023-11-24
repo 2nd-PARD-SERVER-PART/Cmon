b371dfa5118df6dd5f2724a62159630b07b1a603
millejuice <potgusdavy@gmail.com>
2023-11-25 05:52:31 +0900

    파일 갯수 count해주는 api 추가

--- src/main/java/com/example/CRUDModule/controller/UploadController.java
+++ src/main/java/com/example/CRUDModule/controller/UploadController.java
@@ -4,21 +4,26 @@ import com.example.CRUDModule.entity.Image;
 import com.example.CRUDModule.repo.ImageRepo;
 import com.example.CRUDModule.service.ImageService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Blob;
@@ -27,6 +32,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequiredArgsConstructor
@@ -61,9 +69,24 @@ public class UploadController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
     }
+
+    @GetMapping("/numOfImages")
+    public ResponseEntity<Integer> getNumOfImages() {
+        Path dirPath = Paths.get(uploadPath);
+        try {
+            long numOfImages = Files.list(dirPath)
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().toLowerCase().endsWith(".jpg") || path.toString().toLowerCase().endsWith(".jpeg"))
+                    .count();
+
+            return ResponseEntity.ok((int)numOfImages);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @GetMapping(value = "/download/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) throws MalformedURLException {
